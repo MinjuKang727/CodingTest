3ee5811384099805e827588456ea2aacad114a90
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2023-03-05 23:08:52 +0900

    Add files via upload

---
+++ Baekjoon/SilverIV_9012.java
@@ -0,0 +1,77 @@
+package zero_base.src.Baekjoon;
+// 괄호
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/*
+입력1:
+6
+(())())
+(((()())()
+(()())((()))
+((()()(()))(((())))()
+()()()()(()()())()
+(()((())()(
+
+출력1:
+NO
+NO
+YES
+NO
+YES
+NO
+
+입력2:
+3
+((
+))
+())(()
+
+출력2:
+NO
+NO
+NO
+ */
+public class SilverIV_9012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+        String[] vpsFlags = new String[T];
+        LinkedList list = new LinkedList();
+
+        for (int i = 0; i < T; i++) {
+            String VPS = sc.nextLine();
+            String VPSFlag = "YES";
+            for (String bracket: VPS.split("")) {
+                if (bracket.equals("(")) {
+                    list.add("(");
+                } else if (!list.isEmpty()) {
+                    if (list.peek().equals("(")) {
+                        list.poll();
+                    } else {
+                        VPSFlag = "NO";
+                        break;
+                    }
+                } else {
+                    VPSFlag = "NO";
+                    break;
+                }
+            }
+
+            if (!list.isEmpty()) {
+                VPSFlag = "NO";
+            }
+
+            vpsFlags[i] = VPSFlag;
+            list.clear();
+        }
+
+        for (String tf: vpsFlags) {
+            System.out.println(tf);
+        }
+
+    }
+}
